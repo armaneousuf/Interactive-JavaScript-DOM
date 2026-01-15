63c76325ecdafaf733d2e1d02b7abaea9edefe8f
armaneousuf <armaneousuf97@gmail.com>
2026-01-15 17:15:46 +0600

    fix: console added

--- TOUR-OF-EVENT/problems/problem-4/app.js
+++ TOUR-OF-EVENT/problems/problem-4/app.js
@@ -1,5 +1,7 @@
 document.querySelector('#sports').addEventListener('click', function (e) {
     const target = e.target;
+    console.log(target.innerText + ' is clicked');
+
     if (target.matches('li')) {
         target.style.backgroundColor = 'purple';
         target.style.color = '#fff'
