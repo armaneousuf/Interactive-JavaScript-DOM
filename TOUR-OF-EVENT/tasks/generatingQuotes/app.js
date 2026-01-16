62a543a57e04b583bb78f5428fc2d98774a62175
armaneousuf <armaneousuf97@gmail.com>
2026-01-16 17:22:24 +0600

    fix: new project started

---
+++ TOUR-OF-EVENT/tasks/generatingQuotes/app.js
@@ -0,0 +1,50 @@
+let quote = document.querySelector('.quote');
+let person = document.querySelector('.person');
+let btn = document.querySelector('.btn');
+
+const quotes = [
+  {
+    quote: "The only way to do great work is to love what you do.",
+    person: "Steve Jobs",
+  },
+  {
+    quote: "Innovation distinguishes between a leader and a follower.",
+    person: "Steve Jobs",
+  },
+  {
+    quote:
+      "The future belongs to those who believe in the beauty of their dreams.",
+    person: "Eleanor Roosevelt",
+  },
+  {
+    quote: "It always seems impossible until it's done.",
+    person: "Nelson Mandela",
+  },
+  {
+    quote: "In the middle of every difficulty lies opportunity.",
+    person: "Albert Einstein",
+  },
+  {
+    quote:
+      "Your time is limited, so don't waste it living someone else's life.",
+    person: "Steve Jobs",
+  },
+  {
+    quote:
+      "Success is not final, failure is not fatal: it is the courage to continue that counts.",
+    person: "Winston Churchill",
+  },
+  {
+    quote:
+      "The only limit to our realization of tomorrow will be our doubts of today.",
+    person: "Franklin D. Roosevelt",
+  },
+  {
+    quote: "Do what you can, with what you have, where you are.",
+    person: "Theodore Roosevelt",
+  },
+  {
+    quote: "Believe you can and you're halfway there.",
+    person: "Theodore Roosevelt",
+  },
+];
